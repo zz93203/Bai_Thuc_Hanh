4745cc717103885dc20b2c2463397fd3e527dacd
zz93203 <hungvip.200339@gmail.com>
2023-04-18 12:07:31 +0700

    BaiThucHanh

--- BaiThucHanhLab3/NhanVien/src/Run.java
+++
@@ -1,8 +0,0 @@
-public class Run {
-    public static void main(String[] argn) throws Exception {
-        NhanVien nv = new NhanVien();
-        nv.Nhapthongtin();
-        nv.Tinhluong();
-        nv.Inthongtin();
-    }
-}
