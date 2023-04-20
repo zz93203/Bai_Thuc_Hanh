72eeb6c028cc204d65c58b65a826cbaece7e9e0f
zz93203 <hungvip.200339@gmail.com>
2023-04-20 09:07:59 +0700

    BaiThucHanh

--- BaiThucHanhLab3/HinhChuNhat/src/Run.java
+++
@@ -1,9 +0,0 @@
-public class Run {
-    public static void main(String[] args) throws Exception {
-        HinhChuNhat hcn = new HinhChuNhat();
-        hcn.Nhapthongtin();
-        hcn.Tinhchuvi();
-        hcn.Tinhdientich();
-        hcn.Inthongtin();
-    }
-}
--- BaiThucHanhLab3/SinhVien/src/Run.java
+++
@@ -1,7 +0,0 @@
-public class Run {
-    public static void main(String[] argn) throws Exception {
-        SinhVien sv = new SinhVien();
-        sv.Nhapthongtin();
-        sv.Inthongtin();
-    }
-}
